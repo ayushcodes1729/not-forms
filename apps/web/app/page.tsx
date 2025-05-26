fd08756f293a22ccc404b9655b83641db872c880
Ayush Kumar <ayushtiwari0803@gmail.com>
2025-05-26 15:10:22 +0530

    init

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Card } from "@repo/ui/card";
+import { Gradient } from "@repo/ui/gradient";
+import { TurborepoLogo } from "@repo/ui/turborepo-logo";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Hi There</h1>
+    </div>
+  );
+}
